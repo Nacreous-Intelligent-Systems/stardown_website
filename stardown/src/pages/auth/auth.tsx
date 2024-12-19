363e13767394f1c7f97549e6e50fb096b039f871
Illiath_Zero <illiathtw@pm.me>
2024-12-19 10:36:28 -0800

    fixed auth

--- stardown/src/pages/auth/auth.tsx
+++ stardown/src/pages/auth/auth.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 type AuthPageProps = {
 
-};
+}
 
 const AuthPage: React.FC<AuthPageProps> = () => {
 
-    return <div>
+    return ( <div>
         <h1> Auth Page </h1>
     </div>
+    );
+
 }
 export default AuthPage;
