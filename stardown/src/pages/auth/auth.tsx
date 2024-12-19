10bc7ddd457a20f58838f86e549fbcdad3f1150b
Illiath_Zero <illiathtw@pm.me>
2024-12-19 10:22:43 -0800

    testing!

--- stardown/src/pages/auth/auth.tsx
+++ stardown/src/pages/auth/auth.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 type AuthPageProps = {
-    
+
 };
 
-const AuthPage:React.FC<AuthPageProps> = () => {
-    
-    return <div>AuthPage Page</div>
+const AuthPage: React.FC<AuthPageProps> = () => {
+
+    return <div>
+        <h1> Auth Page </h1>
+    </div>
 }
 export default AuthPage;
