72d129ad4dbef1f7e616ae4707187debed3049f1
MK <maxkrawarik@outlook.com>
2025-01-07 21:15:03 -0800

    testing 3

--- stardownwebsite/src/pages/testpage.tsx
+++ stardownwebsite/src/pages/testpage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
 type testpageProps = {
     
 };
 
 const testpage:React.FC<testpageProps> = () => {
     
-    return <div style={{ color: 'brand-orange' }}>
+    return <div style={{ color: 'brand-orange-s' }}>
     This text is blue.
   </div>
 
