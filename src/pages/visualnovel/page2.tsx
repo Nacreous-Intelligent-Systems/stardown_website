6a4a4322686472f9ab026e84d3a47acdc0c85a5e
MK <maxkrawarik@outlook.com>
2025-01-27 07:03:08 -0800

    Page Cycler Testing

--- src/pages/visualnovel/page2.tsx
+++ src/pages/visualnovel/page2.tsx
@@ -8,7 +8,7 @@ type page2Props = {
 
 const page2: React.FC<page2Props> = () => {
 
-    return (<div>``
+    return (<div>
         <h1>Page 2</h1>
         <img src="/Screenshot-2025-01-23-104607.png" alt="Page 2" />
         <VNPageCycler />
--- src/pages/visualnovel/page3.tsx
+++ src/pages/visualnovel/page3.tsx
@@ -1,11 +1,18 @@
+import VNPageCycler from '@/components/VNPageCycler/VNPageCycler';
 import React from 'react';
 
+
 type page3Props = {
-    
+
 };
 
-const page3:React.FC<page3Props> = () => {
-    
-    return <div>page3</div>
+const page3: React.FC<page3Props> = () => {
+
+    return (
+        <div>
+            <h1>Page 3</h1>
+                <VNPageCycler />
+            </div>
+        );
 }
 export default page3;
